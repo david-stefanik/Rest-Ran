c12a768452734b1067b8d54adc6f985a788f40c5
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-03-14 19:53:27 -0700

    teller

---
+++ views/places/index.jsx
@@ -0,0 +1,19 @@
+// GET /places
+app.get('/', (req, res) => {
+    let places = []
+    res.render('places/index')
+  })
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'http://placekitten.com/250/250'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'http://placekitten.com/250/250'
+  }]
+  
