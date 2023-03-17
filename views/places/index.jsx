2c50e5462e4d0a6dc6a63e6a01240d1b8187db0b
David Stefank <113402850+david-stefanik@users.noreply.github.com>
2023-03-16 20:12:45 -0700

    maby

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,19 +1,28 @@
-// GET /places
-app.get('/', (req, res) => {
-    let places = []
-    res.render('places/index')
+let react = require(`react`)
+
+
+
+
+
+
+ 
+function index (data) {
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div>
+        <h2>{place.name}</h2>
+        <img src={place.pic} alt={place.name}/>
+      </div>
+    )
   })
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }]
-  
+  return (
+    <Def>
+        <main>
+            <h1>PLACES INDEX PAGE</h1>
+            {placesFormatted}
+        </main>
+    </Def>
+)
+}
+
+  module.exports = Index
